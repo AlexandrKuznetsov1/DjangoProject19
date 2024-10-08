e766701b020bf738660bcc26a59edcc894db654e
AlexandrKuznetsov1 <1@example.com>
2024-10-08 17:05:25 +0700

    созданы модели БД: 1.Buyer - модель представляющая покупателя. Обладает следующими полями: name - имя покупателя(username аккаунта) balance - баланс(DecimalField) age - возраст. 2.Game - модель представляющая игру. Обладает следующими полями: title - название игры cost - цена(DecimalField) size - размер файлов игры(DecimalField) description - описание(неограниченное кол-во текста) age_limited - ограничение возраста 18+ (BooleanField, по умолчанию False) buyer - покупатель обладающий игрой (ManyToManyField). У каждого покупателя может быть игра и у каждой игры может быть несколько обладателей.

--- module19django/task1/models.py
+++ module19django/task1/models.py
@@ -2,3 +2,21 @@ from django.db import models
 
 
 # Create your models here.
+class Buyer(models.Model):
+    name = models.CharField(max_length=50)
+    balance = models.DecimalField()
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=100)
+    cost = models.DecimalField()
+    size = models.DecimalField()
+    description = models.TextField()
+    age_limited = models.BooleanField(1)
+    buyer = models.ManyToManyField(Buyer, related_name='game')
+
+
