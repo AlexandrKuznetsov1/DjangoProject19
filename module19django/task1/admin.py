37d6e885e1b61c31293cb7bd7519a93cd8e751ca
AlexandrKuznetsov1 <1@example.com>
2024-10-11 11:37:32 +0700

    Домашнее задание по теме "Сайт администрирования"

--- module19django/task1/admin.py
+++ module19django/task1/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    # расшериваем поля для редактирования в панель администрирования
+    list_display = ('name', 'balance', 'age',)
+    # добавляем строку поиска по имени
+    search_fields = ('name',)
+    # фильтр сортировки по балансу
+    list_filter = ('balance',)
+    # настройка внутри панели
+    fields = [('name', 'balance', 'age',),]
+    # секции
+    # fieldsets = (
+    #     ('Buyer',{'fields':
+    #                   ('name', 'balance', 'age',)}))
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    # расшериваем поля для редактирования в панель администрирования
+    list_display = ('title', 'description', 'cost', 'size', 'age_limited',)
+    # добавляем строку поиска по названию
+    search_fields = ('title',)
+    # фильтр сортировки по цене
+    list_filter = ('cost',)
+    # настройка внутри панели
+    fields = [('title', 'description', 'cost', 'size', 'age_limited',),]
+    # секции
+    # fieldsets = ((
+    #     ('Game',{'fields':
+    #         ('title', 'description', 'cost', 'size',)})),
+    #              ('GameLimited', {'fields':('title', 'description', 'age_limited',)}))
