f935a3afce5e388427077e979a891ff09cd9822b
AlexandrKuznetsov1 <1@example.com>
2024-10-17 19:24:23 +0700

    Добавлено ДЗ по теме по теме "Настраиваем СУБД postgre в django"

--- module19django/module19django/settings.py
+++ module19django/module19django/settings.py
@@ -79,10 +79,22 @@ WSGI_APPLICATION = 'module19django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'alexandr_kuznecov',
+        'USER': 'postgres',
+        'PASSWORD': 'Cfyz123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
--- module19django/myblog/models.py
+++ module19django/myblog/models.py
@@ -2,10 +2,20 @@ from django.db import models
 
 
 # Create your models here.
+
+class ReaderPost(models.Model):
+    name = models.CharField(max_length=30)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
     title = models.CharField(max_length=70)
     content = models.TextField()
     create_at = models.DateTimeField(auto_now_add=True)  # дата создания
+    reader_post = models.ManyToManyField(ReaderPost, related_name='myblogs')
 
     def __str__(self):
         return self.title
